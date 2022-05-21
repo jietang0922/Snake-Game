06ea2d777ebefa6c89614c5484e04f7ee805cceb
SKNat26 <jiexuant0922@gmail.com>
2022-05-21 10:48:40 -0400

    added "american" and "christmas" skins

--- snake.js
+++ snake.js
@@ -34,17 +34,34 @@ class Snake {
         this.direction = 0;
         this.begin = 5;
         this.score = 0;
-        if(this.skin == 1) {
-            for(let color of Object.values(this.skins["random"])) {
-                this.skinning.push(color);
-            }
+        switch(this.skin) {
+            case 1:
+                for(let color of Object.values(this.skins["random"])) {
+                    this.skinning.splice(Math.floor(Math.random() * this.skinning.length), 0, color);
+                }
+                break;
+            default:
+                let findPattern = true;
+                let pattern, index = 0;
+                for(let color of Object.values(Object.values(this.skins)[this.skin - 1])) {
+                    if(findPattern) {
+                        findPattern = false;
+                        pattern = color;
+                        continue;
+                    }
+                    for(let i = 0; i < pattern[index]; i++) {
+                        this.skinning.push(color);
+                    }
+                }
+                break;
         }
+
         this.skinner = [...this.skinning];
         for(let i = 0; i < this.begin; i++) {
-            let color = this.skinner.splice(Math.floor(Math.random() * this.skinner.length), 1)[0];
+            let color = this.skinner.shift();
             let square = new Square(260 + 20 * i, 260, color[0], color[1], color[2]);
             this.squares.push(square);
-            if(this.skinner.length == 0) this.skinner = this.skinning;
+            if(this.skinner.length == 0) this.skinner = [...this.skinning];
         }
         this.head = new Head(this.squares[0].x + this.squares[0].w / 2, this.squares[0].y + this.squares[0].l / 2, 10);
     }
@@ -61,7 +78,7 @@ class Snake {
     grow() {
         let x = 2 * this.squares[this.squares.length - 1].x - this.squares[this.squares.length - 2].x;
         let y = 2 * this.squares[this.squares.length - 1].y - this.squares[this.squares.length - 2].y;
-        let color = this.skinner.splice(Math.floor(Math.random() * this.skinner.length), 1)[0];
+        let color = this.skinner.shift()
         let square = new Square(x, y, color[0], color[1], color[2]);
         this.squares.push(square);
         if(this.skinner.length == 0) this.skinner = [...this.skinning];
